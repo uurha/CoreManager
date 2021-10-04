d504deab699d47ad0d3ff7dc748c840c24c58443
uurha <uurha2man@gmail.com>
2021-10-04 23:16:59 +0300

    Apply file layout and code style

--- Assets/CorePlugin/Samples/Scripts/Demo/CrossSceneDataSenderDemo.cs
+++ Assets/CorePlugin/Samples/Scripts/Demo/CrossSceneDataSenderDemo.cs
@@ -1,20 +1,19 @@
 ﻿#region license
 
-// Copyright 2021 Arcueid Elizabeth D'athemon
-// Licensed under the Apache License, Version 2.0 (the "License"); 
-// you may not use this file except in compliance with the License. 
-// You may obtain a copy of the License at 
-//     http://www.apache.org/licenses/LICENSE-2.0 
-// Unless required by applicable law or agreed to in writing, software 
-// distributed under the License is distributed on an "AS IS" BASIS, 
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-// See the License for the specific language governing permissions and 
+// Copyright 2021 - 2021 Arcueid Elizabeth D'athemon
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 #endregion
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using CorePlugin.Attributes.Validation;
 using CorePlugin.Cross.Events.Interface;
@@ -35,6 +34,9 @@ namespace CorePlugin.Samples.Scripts.Demo
 
         private readonly bool[] _isFieldsValid = new bool[3];
 
+        private event CustomEventTypes.DataParsedDelegate DataParsedEvent;
+        private event CustomEventTypes.IsValidDataParsedDelegate IsValidDataParsedEvent;
+
         private void Awake()
         {
             intField.onValueChanged.AddListener(IntValidate);
@@ -42,39 +44,12 @@ namespace CorePlugin.Samples.Scripts.Demo
             stringField.onValueChanged.AddListener(StringValidate);
         }
 
-        public void InvokeEvents()
+        private void CheckValidity()
         {
-            IsValidDataParsedEvent?.Invoke(false);
-        }
-
-        public void Subscribe(params Delegate[] subscribers)
-        {
-            EventExtensions.Subscribe(ref DataParsedEvent, subscribers);
-            EventExtensions.Subscribe(ref IsValidDataParsedEvent, subscribers);
-        }
-
-        public void Unsubscribe(params Delegate[] subscribers)
-        {
-            EventExtensions.Unsubscribe(ref DataParsedEvent, subscribers);
-            EventExtensions.Unsubscribe(ref IsValidDataParsedEvent, subscribers);
-        }
-
-        private event CustomEventTypes.DataParsedDelegate DataParsedEvent;
-        private event CustomEventTypes.IsValidDataParsedDelegate IsValidDataParsedEvent;
-
-        private void IntValidate(string data)
-        {
-            if (!int.TryParse(data, out var intData))
-            {
-                intField.textComponent.color = Color.red;
-                _isFieldsValid[0] = false;
-                CheckValidity();
-                return;
-            }
-            intField.textComponent.color = Color.black;
-            _isFieldsValid[0] = true;
-            _data.IntData = intData;
-            CheckValidity();
+            var isValid = _isFieldsValid.All(x => x);
+            IsValidDataParsedEvent?.Invoke(isValid);
+            if (!isValid) return;
+            DataParsedEvent?.Invoke(_data);
         }
 
         private void ColorValidate(string data)
@@ -95,6 +70,21 @@ namespace CorePlugin.Samples.Scripts.Demo
             CheckValidity();
         }
 
+        private void IntValidate(string data)
+        {
+            if (!int.TryParse(data, out var intData))
+            {
+                intField.textComponent.color = Color.red;
+                _isFieldsValid[0] = false;
+                CheckValidity();
+                return;
+            }
+            intField.textComponent.color = Color.black;
+            _isFieldsValid[0] = true;
+            _data.IntData = intData;
+            CheckValidity();
+        }
+
         private void StringValidate(string data)
         {
             if (data.Length <= 0)
@@ -108,12 +98,21 @@ namespace CorePlugin.Samples.Scripts.Demo
             CheckValidity();
         }
 
-        private void CheckValidity()
+        public void InvokeEvents()
         {
-            var isValid = _isFieldsValid.All(x => x);
-            IsValidDataParsedEvent?.Invoke(isValid);
-            if (!isValid) return;
-            DataParsedEvent?.Invoke(_data);
+            IsValidDataParsedEvent?.Invoke(false);
+        }
+
+        public void Subscribe(params Delegate[] subscribers)
+        {
+            EventExtensions.Subscribe(ref DataParsedEvent, subscribers);
+            EventExtensions.Subscribe(ref IsValidDataParsedEvent, subscribers);
+        }
+
+        public void Unsubscribe(params Delegate[] subscribers)
+        {
+            EventExtensions.Unsubscribe(ref DataParsedEvent, subscribers);
+            EventExtensions.Unsubscribe(ref IsValidDataParsedEvent, subscribers);
         }
     }
 
--- Assets/CorePlugin/Samples/Scripts/SceneSwitcher.cs
+++ Assets/CorePlugin/Samples/Scripts/SceneSwitcher.cs
@@ -1,20 +1,19 @@
 ﻿#region license
 
-// Copyright 2021 Arcueid Elizabeth D'athemon
-// Licensed under the Apache License, Version 2.0 (the "License"); 
-// you may not use this file except in compliance with the License. 
-// You may obtain a copy of the License at 
-//     http://www.apache.org/licenses/LICENSE-2.0 
-// Unless required by applicable law or agreed to in writing, software 
-// distributed under the License is distributed on an "AS IS" BASIS, 
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-// See the License for the specific language governing permissions and 
+// Copyright 2021 - 2021 Arcueid Elizabeth D'athemon
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//     http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
 // limitations under the License.
 
 #endregion
 
 using System;
-using System.Collections.Generic;
 using CorePlugin.Cross.Events.Interface;
 using CorePlugin.Cross.SceneData;
 using CorePlugin.Samples.Scripts.EventTypes;
@@ -38,10 +37,9 @@ namespace CorePlugin.Samples.Scripts
             loadButton.onClick.AddListener(LoadScene);
         }
 
-        public Delegate[] GetSubscribers()
+        private void DataReceiver(DataTransfer data)
         {
-            var delegates = new Delegate[] {(CustomEventTypes.IsValidDataParsedDelegate) IsDataValid, (CustomEventTypes.DataParsedDelegate) DataReceiver};
-            return delegates;
+            _data = data;
         }
 
         private void IsDataValid(bool isValid)
@@ -51,16 +49,17 @@ namespace CorePlugin.Samples.Scripts
             _data = null;
         }
 
-        private void DataReceiver(DataTransfer data)
-        {
-            _data = data;
-        }
-
         private void LoadScene()
         {
             if (_data == null) return;
             SceneDataHandler.Instance.AddData(_data);
-            SceneLoader.LoadSceneAsync(scene, new LoadSceneOptions{UseIntermediate = true, SceneLoadMode = LoadSceneMode.Single});
+            SceneLoader.LoadSceneAsync(scene, new LoadSceneOptions {UseIntermediate = true, SceneLoadMode = LoadSceneMode.Single});
+        }
+
+        public Delegate[] GetSubscribers()
+        {
+            var delegates = new Delegate[] {(CustomEventTypes.IsValidDataParsedDelegate) IsDataValid, (CustomEventTypes.DataParsedDelegate) DataReceiver};
+            return delegates;
         }
     }
 }
